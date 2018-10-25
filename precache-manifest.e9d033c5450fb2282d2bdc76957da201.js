4fafbfe49887ad51571d1eb9f4b0091a581262f8
Jinyeong Jung <jinyeong32@gmail.com>
2018-10-25 13:51:52 +0900

    Updates

---
+++ precache-manifest.e9d033c5450fb2282d2bdc76957da201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "b6958e96c39d41ccb4b1",
+    "url": "/movie_app/static/js/main.b6958e96.chunk.js"
+  },
+  {
+    "revision": "392d6e18dd1fb16cb5e2",
+    "url": "/movie_app/static/js/1.392d6e18.chunk.js"
+  },
+  {
+    "revision": "b6958e96c39d41ccb4b1",
+    "url": "/movie_app/static/css/main.0c018ee4.chunk.css"
+  },
+  {
+    "revision": "6068a6077b0cc745e455ec5ddf88a64b",
+    "url": "/movie_app/index.html"
+  }
+];
